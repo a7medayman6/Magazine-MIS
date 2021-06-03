61072c84b7229c071a437ad260b502252cfea3c5
Ahmed Ayman <a.ayman6000@gmail.com>
2021-06-03 23:00:13 +0200

    added login, home, editions, and managers forms

---
+++ Magazino/Editions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// UNCOMMENT AFTER CONNECTING TO DB
+
+// using Oracle.DataAccess.Client;
+// using Oracle.DataAccess.Types;
+
+namespace Magazino
+{
+    public partial class Editions : Form
+    {
+        // uncomment this after connecting to oracle database
+        // edit the connection string to match the database data
+        // connection string
+        /*
+        string connString = "Data Source=ORCL; User Id=scott;Password=tiger;";
+        OracleConnection conn;
+        */
+        public Editions()
+        {
+            InitializeComponent();
+        }
+
+        private void Editions_Load(object sender, EventArgs e)
+        {
+            // uncomment this after connecting to oracle database
+            // CONNECTING TO THE DATABASE
+            /*
+            conn = new OracleConnection(connString);
+            conn.Open();
+            */
+            // uncomment this after connecting to oracle database
+            // login validation from database
+
+            /*
+            OracleCommand cmd = new OracleCommand(); 
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT Manager_Id, First_name, Last_name FROM MANAGERS WHERE Email=:email AND Pass=:pass;"; 
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("email", email);
+            cmd.Parameters.Add("pass", pass);
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                
+            }
+            */
+}
+}
+}
---
+++ Magazino/Managers.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Magazino
+{
+    partial class Managers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gridManagers = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridManagers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Magazino.Properties.Resources.power_button_off;
+            this.pictureBox1.Location = new System.Drawing.Point(332, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // gridManagers
+            // 
+            this.gridManagers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridManagers.Location = new System.Drawing.Point(30, 108);
+            this.gridManagers.Name = "gridManagers";
+            this.gridManagers.RowHeadersWidth = 51;
+            this.gridManagers.RowTemplate.Height = 24;
+            this.gridManagers.Size = new System.Drawing.Size(358, 388);
+            this.gridManagers.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(124)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(49, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Managers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(419, 607);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gridManagers);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Managers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Managers";
+            this.Load += new System.EventHandler(this.Managers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridManagers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView gridManagers;
+        private System.Windows.Forms.Button button1;
+    }
+}
